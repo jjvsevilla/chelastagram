f937066df994b5226d0b45fc2250b139c46ad052
juan vento <j.j.v.sevilla@gmail.com>
2017-10-26 01:33:19 -0500

    add NoMatch component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import ChelaGrid from './ChelaGrid';
 import ChelaView from './ChelaView';
+import NoMatch from './NoMatch';
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={ChelaGrid} />
     <Route path="/view/:chelaId" component={ChelaView} />
+    <Route component={NoMatch} />
   </Switch>
 );
 
